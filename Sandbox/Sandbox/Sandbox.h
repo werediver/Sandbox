2ca3c2bdeecc6e9dd81b6c5fb60b2607b510f693
Raman Fedaseyeu <werediver@gmail.com>
2018-04-22 15:59:36 +0200

    Clear workspace

--- Sandbox/Sandbox/Sandbox.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for Sandbox.
-FOUNDATION_EXPORT double SandboxVersionNumber;
-
-//! Project version string for Sandbox.
-FOUNDATION_EXPORT const unsigned char SandboxVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Sandbox/PublicHeader.h>
